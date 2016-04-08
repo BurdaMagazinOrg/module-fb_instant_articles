b8be5785de887c800171e78ce017e507d0efed22
Scott Rigby <scott@r6by.com>
2016-04-08 14:22:42 -0400

    Revert "Fixes additional Javascript formatting issues reported by parereview."
    This reverts commit cf2f534e4c03d822f701c819c62f3fbad4346983.

--- js/admin.js
+++ js/admin.js
@@ -3,77 +3,74 @@
  * Javascript functionality for Display Suite's administration UI.
  */
 
-(function ($) {
-  'use strict';
+(function($) {
+/**
+ * Row handlers for the 'Manage display' screen.
+ */
+Drupal.fieldUIDisplayOverview = Drupal.fieldUIDisplayOverview || {};
+
+Drupal.fieldUIDisplayOverview.facebookInstantArticles = function (row, data) {
+
+  this.row = row;
+  this.name = data.name;
+  this.region = data.region;
+  this.tableDrag = data.tableDrag;
+
+  // Attach change listener to the 'region' select.
+  this.$regionSelect = $('select.ds-field-region', row);
+  this.$regionSelect.change(Drupal.fieldUIOverview.onChange);
+
+  // Attach change listener to the 'formatter type' select.
+  this.$formatSelect = $('select.field-formatter-type', row);
+  this.$formatSelect.change(Drupal.fieldUIOverview.onChange);
+
+  return this;
+};
+
+Drupal.fieldUIDisplayOverview.facebookInstantArticles.prototype = {
 
   /**
-   * Row handlers for the 'Manage display' screen.
+   * Returns the region corresponding to the current form values of the row.
    */
-  Drupal.fieldUIDisplayOverview = Drupal.fieldUIDisplayOverview || {};
+  getRegion: function () {
+    return this.$regionSelect.val();
+  },
 
-  Drupal.fieldUIDisplayOverview.facebookInstantArticles = function (row, data) {
+  /**
+   * Reacts to a row being changed regions.
+   *
+   * This function is called when the row is moved to a different region, as a
+   * result of either :
+   * - a drag-and-drop action
+   * - user input in one of the form elements watched by the
+   *   Drupal.fieldUIOverview.onChange change listener.
+   *
+   * @param region
+   *   The name of the new region for the row.
+   * @return
+   *   A hash object indicating which rows should be AJAX-updated as a result
+   *   of the change, in the format expected by
+   *   Drupal.displayOverview.AJAXRefreshRows().
+   */
+  regionChange: function (region) {
 
-    this.row = row;
-    this.name = data.name;
-    this.region = data.region;
-    this.tableDrag = data.tableDrag;
+     // Replace dashes with underscores.
+     region = region.replace(/-/g, '_');
 
-    // Attach change listener to the 'region' select.
-    this.$regionSelect = $('select.ds-field-region', row);
-    this.$regionSelect.change(Drupal.fieldUIOverview.onChange);
+     // Set the region of the select list.
+     this.$regionSelect.val(region);
 
-    // Attach change listener to the 'formatter type' select.
-    this.$formatSelect = $('select.field-formatter-type', row);
-    this.$formatSelect.change(Drupal.fieldUIOverview.onChange);
+     // Prepare rows to be refreshed in the form.
+     var refreshRows = {};
+     refreshRows[this.name] = this.$regionSelect.get(0);
 
-    return this;
-  };
+     // If a row is handled by field_group module, loop through the children.
+     if ($(this.row).hasClass('field-group') && $.isFunction(Drupal.fieldUIDisplayOverview.group.prototype.regionChangeFields)) {
+       Drupal.fieldUIDisplayOverview.group.prototype.regionChangeFields(region, this, refreshRows);
+     }
 
-  Drupal.fieldUIDisplayOverview.facebookInstantArticles.prototype = {
-
-    /**
-     * Returns the region corresponding to the current form values of the row.
-     */
-    getRegion: function () {
-      return this.$regionSelect.val();
-    },
-
-    /**
-     * Reacts to a row being changed regions.
-     *
-     * This function is called when the row is moved to a different region, as a
-     * result of either :
-     * - a drag-and-drop action
-     * - user input in one of the form elements watched by the
-     *   Drupal.fieldUIOverview.onChange change listener.
-     *
-     * @param region
-     *   The name of the new region for the row.
-     *
-     * @return
-     *   A hash object indicating which rows should be AJAX-updated as a result
-     *   of the change, in the format expected by
-     *   Drupal.displayOverview.AJAXRefreshRows().
-     */
-    regionChange: function (region) {
-
-      // Replace dashes with underscores.
-      region = region.replace(/-/g, '_');
-
-      // Set the region of the select list.
-      this.$regionSelect.val(region);
-
-      // Prepare rows to be refreshed in the form.
-      var refreshRows = {};
-      refreshRows[this.name] = this.$regionSelect.get(0);
-
-      // If a row is handled by field_group module, loop through the children.
-      if ($(this.row).hasClass('field-group') && $.isFunction(Drupal.fieldUIDisplayOverview.group.prototype.regionChangeFields)) {
-        Drupal.fieldUIDisplayOverview.group.prototype.regionChangeFields(region, this, refreshRows);
-      }
-
-      return refreshRows;
-    }
-  };
+     return refreshRows;
+  }
+};
 
 })(jQuery);
