890555a579b6566cc22960dd999352cbabc00355
Matthew Oliveira <m4olivei@gmail.com>
2016-04-08 09:17:51 -0400

    Issue #2702667 by m4olivei: Cleanup the view row plugin.

--- modules/fb_instant_articles_views/theme/views-view-row-fia.tpl.php
+++ modules/fb_instant_articles_views/theme/views-view-row-fia.tpl.php
@@ -1,38 +1,18 @@
 <?php
+
 /**
  * @file
- * Default theme implementation to display an item in a views FIA (facebook instant articles) feed.
+ * Default theme implementation to display an item in a Facebook Instant
+ * Articles feed.
  *
  * Available variables:
- * - options['title']: RSS item title.
- * - options['subtitle']: RSS item subtitle.
- * - options['kicker']: teaser short text for the article
- * - options['created']: created datetime
- * - options['modified']: modified datetime
- * - options['link']: RSS item link.
- * - options['figure']: a full html markup for a header image (syntax below)
- * - options['authors'] : htmls <address> tags for article authors (syntax below)
- *
- * - content : HTML content for the article
- *
- * - footer : HTML content for the article footer
- *
- * The syntax for the figure allows:
- *   <figure>
- *       <img src="http://mydomain.com/path/to/img.jpg" />
- *       <figcaption>This image is amazing</figcaption>
- *   </figure>
- *
- * The Syntax for the author <address>:
- *      <address>
- *       <a rel="facebook" href="http://facebook.com/brandon.diamond">Brandon Diamond</a>
- *       Brandon is a avid zombie hunter.
- *     </address>
- *     <address>
- *       <a>TR Vishwanath</a>
- *       Vish is a scholar and a gentleman.
- *     </address>
- *
+ * - $title: RSS item title.
+ * - $link: canonical URL for this item.
+ * - $guid: globally unique id for this item.
+ * - $content: the fully rendered instant article markup.
+ * - $author: the author of the article.
+ * - $created: date created timestamp in ISO 8601 format.
+ * - $modified: date modified timestamp in ISO 8601 format.
  *
  * @see template_preprocess_views_view_row_fia()
  *
@@ -40,15 +20,15 @@
  */
 ?>
 <item>
-  <title><?php print $options['title']; ?></title>
-  <link><?php print $options['link']; ?></link>
-  <guid><?php print $options['guid']; ?></guid>
-  <pubDate><?php print $options['created']; ?></pubDate>
-  <author><?php print $options['author']; ?></author>
+  <title><?php print $title; ?></title>
+  <link><?php print $link; ?></link>
+  <guid><?php print $guid; ?></guid>
+  <pubDate><?php print $created; ?></pubDate>
+  <author><?php print $author; ?></author>
   <content:encoded>
     <![CDATA[
     <!-- Article body started -->
-      <?php print $row ?>
+      <?php print $content; ?>
     ]]>
   </content:encoded>
 </item>
