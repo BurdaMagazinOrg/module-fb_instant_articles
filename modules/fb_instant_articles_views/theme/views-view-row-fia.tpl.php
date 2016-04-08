c1a85b05101d6cf58b9ff9e7d38d52dbb74e5bd5
Matthew Oliveira <m4olivei@gmail.com>
2016-04-08 10:34:17 -0400

    Issue #2702667 by m4olivei: Break up the required and optional <item> elements so that the optional ones are more easily added to in a preprocessor.

--- modules/fb_instant_articles_views/theme/views-view-row-fia.tpl.php
+++ modules/fb_instant_articles_views/theme/views-view-row-fia.tpl.php
@@ -8,11 +8,8 @@
  * Available variables:
  * - $title: RSS item title.
  * - $link: canonical URL for this item.
- * - $guid: globally unique id for this item.
  * - $content: the fully rendered instant article markup.
- * - $author: the author of the article.
- * - $created: date created timestamp in ISO 8601 format.
- * - $modified: date modified timestamp in ISO 8601 format.
+ * - $item_elements: additional optional <item> child tags.
  *
  * @see template_preprocess_views_view_row_fia()
  *
@@ -22,13 +19,11 @@
 <item>
   <title><?php print $title; ?></title>
   <link><?php print $link; ?></link>
-  <guid><?php print $guid; ?></guid>
-  <pubDate><?php print $created; ?></pubDate>
-  <author><?php print $author; ?></author>
   <content:encoded>
     <![CDATA[
     <!-- Article body started -->
       <?php print $content; ?>
     ]]>
   </content:encoded>
+  <?php print $item_elements; ?>
 </item>
