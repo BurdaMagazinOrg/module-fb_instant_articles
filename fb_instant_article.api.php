24a1188fb5da1b688095f5b96fcdb336642936ca
Scott Rigby <scott@r6by.com>
2016-04-29 14:15:19 -0400

    Issue #2703063: Add  param to hook_fb_instant_articles_article_alter()

--- fb_instant_article.api.php
+++ fb_instant_article.api.php
@@ -19,11 +19,27 @@
  *
  * @param \Facebook\InstantArticles\Elements\InstantArticle $instantArticle
  *   The SDK InstantArticle object before it is retrieved from ArticleWrapper.
+ * @param array $context
+ *   An associative array of all contextual information for use in altering the
+ *   InstantArticle object. When interacting with ArticleWrapper directly,
+ *   modules may set additional context with ArticleWrapper::setContext().
  *
  * @see \Drupal\fb_instant_articles\ArticleWrapper::getArticle()
+ * @see \Drupal\fb_instant_articles\ArticleWrapper::setContext()
  */
-function hook_fb_instant_articles_article_alter(\Facebook\InstantArticles\Elements\InstantArticle $instantArticle) {
-  // @todo Provide useful examples here or a better link.
+function hook_fb_instant_articles_article_alter($instantArticle, $context) {
+  // Check to see if a module like fb_instant_articles_display has set Entity
+  // context.
+  if (isset($context['entity_type']) && isset($context['entity'])) {
+    // Example: Set the author using a custom callback.
+    $byline = custom_fb_instant_articles_byline_callback($context['entity_type'], $context['entity']);
+    $instantArticle
+      ->getHeader()
+      ->addAuthor(
+        \Facebook\InstantArticles\Elements\Author::create()
+          ->withName($byline)
+      );
+  }
 }
 
 /**
--- src/ArticleWrapper.php
+++ src/ArticleWrapper.php
@@ -29,6 +29,17 @@ class ArticleWrapper {
   private $instantArticle;
 
   /**
+   * Stores contextual information for altering the InstantArticle object.
+   *
+   * @see hook_fb_instant_articles_article_alter()
+   * @see setContext()
+   * @see getContext()
+   *
+   * @var array
+   */
+  private $context = array();
+
+  /**
    * ArticleWrapper constructor.
    *
    * Instantiates the InstantArticle object, and adds Drupal Base module configs
@@ -68,8 +79,45 @@ class ArticleWrapper {
    * @return \Facebook\InstantArticles\Elements\InstantArticle
    */
   public function getArticle() {
-    drupal_alter('fb_instant_articles_article', $this->instantArticle);
+    drupal_alter('fb_instant_articles_article', $this->instantArticle, $this->context);
     return $this->instantArticle;
+  }
+
+  /**
+   * Sets context for altering the InstantArticle object.
+   *
+   * @param string $key
+   * @param mixed $value
+   *
+   * @see $context
+   * @see hook_fb_instant_articles_article_alter()
+   * @see getContext()
+   */
+  public function setContext($key, $value) {
+    $this->context[$key] = $value;
+  }
+
+  /**
+   * Gets context used to alter the InstantArticle object.
+   *
+   * @param string $key
+   *   Optional context array key.
+   *
+   * @return array
+   *   If $key is passed, the context array key's value. If $key is NULL, an
+   *   associative array of all contextual information for use in altering the
+   *   InstantArticle object.
+   *
+   * @see $context
+   * @see hook_fb_instant_articles_article_alter()
+   * @see setContext()
+   */
+  public function getContext($key = NULL) {
+    if (isset($key)) {
+      return isset($this->context[$key]) ? $this->context[$key] : NULL;
+    }
+
+    return $this->context;
   }
 
   /**
