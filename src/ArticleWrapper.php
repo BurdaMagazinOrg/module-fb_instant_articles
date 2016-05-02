98ee45747d6e8c30f88e6516e1c8e1c4aff0b8e8
Scott Rigby <scott@r6by.com>
2016-05-02 16:17:16 -0400

    Issue #2703063: Move hook_fb_instant_articles_instant_article_alter from getArticle() to create() to avoid duplicate calls (by Disiplay module calling getArticle for every field handler)

--- src/ArticleWrapper.php
+++ src/ArticleWrapper.php
@@ -29,21 +29,17 @@ class ArticleWrapper {
   private $instantArticle;
 
   /**
-   * Stores contextual information for altering the InstantArticle object.
-   *
-   * @see hook_fb_instant_articles_article_alter()
-   * @see setContext()
-   * @see getContext()
-   *
-   * @var array
-   */
-  private $context = array();
-
-  /**
    * ArticleWrapper constructor.
    *
-   * Instantiates the InstantArticle object, and adds Drupal Base module configs
-   * where appropriate.
+   * Instantiates the InstantArticle object, adding Drupal Base module configs
+   * where appropriate. Also invokes a hook to allow other modules to alter the
+   * InstantArticle object before render or any other operation.
+   *
+   * @param array $context
+   *   An associative array of contextual information altering the
+   *   InstantArticle object.
+   *
+   * @see hook_fb_instant_articles_instant_article_alter()
    *
    * Note modules making use of this wrapper must set the required Canonical URL
    * with:
@@ -51,73 +47,34 @@ class ArticleWrapper {
    * ArticleWrapper::create()->getArticle()->withCanonicalUrl($url);
    * @endcode
    */
-  private function __construct() {
+  private function __construct($context = array()) {
     $this->instantArticle = InstantArticle::create()
       ->addMetaProperty('op:generator:application', 'drupal/fb_instant_articles')
       ->addMetaProperty('op:generator:application:version', self::getApplicationVersion())
       ->withStyle(variable_get('fb_instant_articles_style', 'default'));
-
+    drupal_alter('fb_instant_articles_article', $this->instantArticle, $context);
   }
 
   /**
    * Creates a Drupal wrapper for an InstantArticle object.
    *
+   * @param array $context
+   *   An associative array of contextual information altering the
+   *   InstantArticle object.
+   *
    * @return \Drupal\fb_instant_articles\ArticleWrapper
    */
-  public static function create() {
-    return new ArticleWrapper();
+  public static function create($context) {
+    return new ArticleWrapper($context);
   }
 
   /**
    * Gets the wrapped InstantArticle object.
    *
-   * Also invokes a hook to allow other modules to alter the InstantArticle
-   * object before render or any other operation.
-   *
-   * @see hook_fb_instant_articles_instant_article_alter()
-   *
    * @return \Facebook\InstantArticles\Elements\InstantArticle
    */
   public function getArticle() {
-    drupal_alter('fb_instant_articles_article', $this->instantArticle, $this->context);
     return $this->instantArticle;
-  }
-
-  /**
-   * Sets context for altering the InstantArticle object.
-   *
-   * @param string $key
-   * @param mixed $value
-   *
-   * @see $context
-   * @see hook_fb_instant_articles_article_alter()
-   * @see getContext()
-   */
-  public function setContext($key, $value) {
-    $this->context[$key] = $value;
-  }
-
-  /**
-   * Gets context used to alter the InstantArticle object.
-   *
-   * @param string $key
-   *   Optional context array key.
-   *
-   * @return array
-   *   If $key is passed, the context array key's value. If $key is NULL, an
-   *   associative array of all contextual information for use in altering the
-   *   InstantArticle object.
-   *
-   * @see $context
-   * @see hook_fb_instant_articles_article_alter()
-   * @see setContext()
-   */
-  public function getContext($key = NULL) {
-    if (isset($key)) {
-      return isset($this->context[$key]) ? $this->context[$key] : NULL;
-    }
-
-    return $this->context;
   }
 
   /**
