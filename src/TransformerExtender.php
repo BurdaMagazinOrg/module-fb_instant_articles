3cadbdb6df13ec8ea981827aeed19486eb2c496e
Matthew Oliveira <m4olivei@gmail.com>
2017-04-26 14:59:07 -0400

    Issue #2871634 Remove autoload property in composer.json, no longer needed

--- src/TransformerExtender.php
+++ src/TransformerExtender.php
@@ -1,26 +1,18 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\fb_instant_articles\TransformerExtender.
- */
-
 namespace Drupal\fb_instant_articles;
 
 use Facebook\InstantArticles\Transformer\Transformer;
 
 /**
  * Encapsulates Drupal-specific logic when using the Transformer class.
- *
- * Class TransformerExtender
- * @package Drupal\fb_instant_articles
  */
 class TransformerExtender extends Transformer {
 
   /**
    * Transformer constructor.
    *
-   * Adds Drupal hook to allow transformer rules to be added by other modules.
+   * Adds a Drupal hook to allow transformer rules to be added by other modules.
    * We invoke the hook in a constructor - as opposed to inside transform() or a
    * new invokeRules() method - because of the way Transformer is normally used.
    *
@@ -49,19 +41,19 @@ class TransformerExtender extends Transformer {
    * @see \Facebook\InstantArticles\Transformer\Transformer::loadRules()
    */
   public function addRules(array $rules) {
-    foreach ($rules as $rules) {
-      $clazz = $rules['class'];
+    foreach ($rules as $rule) {
+      $clazz = $rule['class'];
       try {
         $factory_method = new \ReflectionMethod($clazz, 'createFrom');
       }
       catch (\ReflectionException $e) {
         $factory_method =
           new \ReflectionMethod(
-            'Facebook\\InstantArticles\\Transformer\\Rules\\'.$clazz,
+            'Facebook\\InstantArticles\\Transformer\\Rules\\' . $clazz,
             'createFrom'
           );
       }
-      $this->addRule($factory_method->invoke(null, $rules));
+      $this->addRule($factory_method->invoke(NULL, $rule));
     }
   }
 
