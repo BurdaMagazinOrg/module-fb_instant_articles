1bcc506346d1d935458e9eef4b2c9e2d5e44a54d
Matthew Oliveira <m4olivei@gmail.com>
2017-06-14 16:16:49 -0400

    Issue #2872807 Fill out the Blockquote field formatter implementation for instant articles

--- src/Plugin/Field/FieldFormatter/BlockquoteFormatter.php
+++ src/Plugin/Field/FieldFormatter/BlockquoteFormatter.php
@@ -2,6 +2,11 @@
 
 namespace Drupal\fb_instant_articles\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\fb_instant_articles\Plugin\Field\InstantArticleFormatterInterface;
+use Facebook\InstantArticles\Elements\Blockquote;
+use Facebook\InstantArticles\Elements\InstantArticle;
+
 /**
  * Plugin implementation of the 'fbia_blockquote' formatter.
  *
@@ -9,10 +14,26 @@ namespace Drupal\fb_instant_articles\Plugin\Field\FieldFormatter;
  *   id = "fbia_blockquote",
  *   label = @Translation("FBIA Blockquote"),
  *   field_types = {
- *     "text",
- *     "text_long",
- *     "text_with_summary",
+ *     "string",
+ *     "string_long"
  *   }
  * )
  */
-class BlockquoteFormatter extends FormatterBase {}
+class BlockquoteFormatter extends FormatterBase implements InstantArticleFormatterInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewInstantArticle(FieldItemListInterface $items, InstantArticle $article, $region, $langcode = NULL) {
+    foreach ($items as $delta => $item) {
+      // Blockquotes are only allowed in the content region, add it there
+      // regardless of the given $region. Note that the FBIA SDK will sanitize
+      // the value.
+      $article->addChild(
+        Blockquote::create()
+          ->appendText($item->value)
+      );
+    }
+  }
+
+}
--- src/Plugin/Field/FieldFormatter/ParagraphFormatter.php
+++ src/Plugin/Field/FieldFormatter/ParagraphFormatter.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\fb_instant_articles\Plugin\Field\FieldFormatter;
 
-use Drupal\Component\Utility\Html;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\fb_instant_articles\Plugin\Field\InstantArticleFormatterInterface;
 use Facebook\InstantArticles\Elements\InstantArticle;
@@ -30,9 +29,12 @@ class ParagraphFormatter extends FormatterBase implements InstantArticleFormatte
    */
   public function viewInstantArticle(FieldItemListInterface $items, InstantArticle $article, $region, $langcode = NULL) {
     foreach ($items as $delta => $item) {
+      // Paragraphs are only allowed in the content region, add it there
+      // regardless of the given $region. Note that the FBIA SDK will sanitize
+      // the value.
       $article->addChild(
         Paragraph::create()
-          ->appendText(Html::escape($item->value))
+          ->appendText($item->value)
       );
     }
   }
