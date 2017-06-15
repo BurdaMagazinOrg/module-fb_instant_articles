cb4740f645e36f580d3e44aabd51874a4e2eb161
Matthew Oliveira <m4olivei@gmail.com>
2017-06-15 09:51:25 -0400

    Issue #2872807 Finish KickerFormatter implementation with support for modifying instant article

--- src/Plugin/Field/FieldFormatter/KickerFormatter.php
+++ src/Plugin/Field/FieldFormatter/KickerFormatter.php
@@ -2,6 +2,11 @@
 
 namespace Drupal\fb_instant_articles\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\fb_instant_articles\Plugin\Field\InstantArticleFormatterInterface;
+use Facebook\InstantArticles\Elements\Header;
+use Facebook\InstantArticles\Elements\InstantArticle;
+
 /**
  * Plugin implementation of the 'fbia_kicker' formatter.
  *
@@ -9,10 +14,29 @@ namespace Drupal\fb_instant_articles\Plugin\Field\FieldFormatter;
  *   id = "fbia_kicker",
  *   label = @Translation("FBIA Kicker"),
  *   field_types = {
- *     "text",
- *     "text_long",
- *     "text_with_summary",
+ *     "string",
+ *     "string_long"
  *   }
  * )
  */
-class KickerFormatter extends FormatterBase {}
+class KickerFormatter extends FormatterBase implements InstantArticleFormatterInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewInstantArticle(FieldItemListInterface $items, InstantArticle $article, $region, $langcode = NULL) {
+    // Kickers are only added to the header. Get the header, creating it if need
+    // be.
+    $header = $article->getHeader();
+    if (!$header) {
+      $header = Header::create();
+      $article->withHeader($header);
+    }
+    // Note that there can only be one kicker. We use the first value as the
+    // kicker.
+    if ($item = $items->get(0)) {
+      $header->withKicker($items->get(0)->value);
+    }
+  }
+
+}
