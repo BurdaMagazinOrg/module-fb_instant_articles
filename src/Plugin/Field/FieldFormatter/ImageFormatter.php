3a2928ec110d50340fb8836f1c1eaa5b3a1cb58d
Matthew Oliveira <m4olivei@gmail.com>
2017-06-14 23:46:59 -0400

    Issue #2872807 Implement instant article image formatter details

--- src/Plugin/Field/FieldFormatter/ImageFormatter.php
+++ src/Plugin/Field/FieldFormatter/ImageFormatter.php
@@ -1,8 +1,16 @@
 <?php
 
 namespace Drupal\fb_instant_articles\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\fb_instant_articles\Plugin\Field\InstantArticleFormatterInterface;
+use Drupal\fb_instant_articles\Regions;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter as DrupalImageFormatter;
+use Facebook\InstantArticles\Elements\Caption;
+use Facebook\InstantArticles\Elements\Header;
 use Facebook\InstantArticles\Elements\Image;
+use Facebook\InstantArticles\Elements\InstantArticle;
 
 /**
  * Plugin implementation of the 'fbia_image' formatter.
@@ -15,7 +23,17 @@ use Facebook\InstantArticles\Elements\Image;
  *   }
  * )
  */
-class ImageFormatter extends FormatterBase {
+class ImageFormatter extends DrupalImageFormatter implements InstantArticleFormatterInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    // Do nothing. Our field formatters are unique in that they are not meant
+    // to generate HTML on their own, but only signal to the Serialization API
+    // the fate of this field in the FBIA document.
+    return [];
+  }
 
   /**
    * {@inheritdoc}
@@ -77,12 +95,17 @@ class ImageFormatter extends FormatterBase {
    */
   public function settingsSummary() {
     $summary = [];
-    $image_styles = image_style_options();
-    if ($image_style = $this->getSetting('image_style')) {
-      $summary[] = $this->t('Image style: @style', ['@style' => $image_styles[$image_style]]);
+    $image_styles = image_style_options(FALSE);
+    // Unset possible 'No defined styles' option.
+    unset($image_styles['']);
+    // Styles could be lost because of enabled/disabled modules that defines
+    // their styles in code.
+    $image_style_setting = $this->getSetting('image_style');
+    if (isset($image_styles[$image_style_setting])) {
+      $summary[] = t('Image style: @style', ['@style' => $image_styles[$image_style_setting]]);
     }
     else {
-      $summary[] = $this->t('Original image');
+      $summary[] = t('Original image');
     }
     if ($this->getSetting('caption')) {
       $summary[] = $this->t('Use alt text as caption');
@@ -97,6 +120,58 @@ class ImageFormatter extends FormatterBase {
       $summary[] = $this->t('Presentation: @presentation', ['@presentation' => $this->presentationLabel($presentation)]);
     }
     return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewInstantArticle(FieldItemListInterface $items, InstantArticle $article, $region, $langcode = NULL) {
+    // Need to call parent::prepareView() to populate the entities since it's
+    // not otherwise getting called.
+    $this->prepareView([$items->getEntity()->id() => $items]);
+
+    /** @var \Drupal\Core\Field\EntityReferenceFieldItemListInterface $items */
+    if (empty($images = $this->getEntitiesToView($items, $langcode))) {
+      // Early opt-out if the field is empty.
+      return;
+    }
+
+    /** @var \Drupal\image\ImageStyleInterface $image_style */
+    $image_style = $this->imageStyleStorage->load($this->getSetting('image_style'));
+    /** @var \Drupal\file\FileInterface[] $images */
+    foreach ($images as $delta => $image) {
+      $image_uri = $image->getFileUri();
+      $url = $image_style ? $image_style->buildUrl($image_uri) : file_create_url($image_uri);
+      $article_image = Image::create()
+        ->withURL($url);
+      if ($this->getSetting('caption') && isset($image->_referringItem) && ($caption = $image->_referringItem->alt)) {
+        $article_image->withCaption(
+          Caption::create()
+            ->appendText($caption)
+        );
+      }
+      if ($this->getSetting('likes')) {
+        $article_image->enableLike();
+      }
+      if ($this->getSetting('comments')) {
+        $article_image->enableComments();
+      }
+      if ($presentation = $this->getSetting('presentation')) {
+        $article_image->withPresentation($presentation);
+      }
+      // Images can either go in the header as the cover image, or in the body.
+      if ($region === Regions::REGION_HEADER) {
+        $header = $article->getHeader();
+        if (!$header) {
+          $header = Header::create();
+          $article->withHeader($header);
+        }
+        $header->withCover($article_image);
+      }
+      else {
+        $article->addChild($article_image);
+      }
+    }
   }
 
   /**
