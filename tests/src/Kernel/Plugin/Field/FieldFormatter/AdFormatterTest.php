aa7df7b820da54dc464a1946f32f28947ca36c36
Matthew Oliveira <m4olivei@gmail.com>
2017-06-16 20:19:32 -0400

    Issue #2872807 Test instant article ad formatter.

---
+++ tests/src/Kernel/Plugin/Field/FieldFormatter/AdFormatterTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\Tests\fb_instant_articles\Kernel\Plugin\Field\FieldFormatter;
+
+use Drupal\entity_test\Entity\EntityTest;
+use Drupal\fb_instant_articles\Plugin\Field\FieldFormatter\FormatterBase;
+use Drupal\fb_instant_articles\Regions;
+use Facebook\InstantArticles\Elements\Ad;
+use Facebook\InstantArticles\Elements\InstantArticle;
+
+/**
+ * Tests the instant article ad formatter.
+ *
+ * @group fb_instant_articles
+ */
+class AdFormatterTest extends FormatterTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Test an ad formatter with the default settings.
+    $this->display->setComponent($this->fieldName, [
+      'type' => 'fbia_ad',
+      'settings' => [],
+    ]);
+    $this->display->save();
+  }
+
+  /**
+   * Test the instant article ad formatter.
+   */
+  public function testAdFormatter() {
+    $ad_url = 'http://example.com/ad';
+
+    $entity = EntityTest::create([]);
+    $entity->{$this->fieldName}->value = $ad_url;
+
+    /** @var \Drupal\fb_instant_articles\Plugin\Field\InstantArticleFormatterInterface $formatter */
+    $formatter = $this->display->getRenderer($this->fieldName);
+    $article = InstantArticle::create();
+    $formatter->viewInstantArticle($entity->{$this->fieldName}, $article, Regions::REGION_HEADER);
+
+    $ads = $article->getHeader()->getAds();
+    $this->assertEquals(1, count($ads));
+    $this->assertEquals(300, $ads[0]->getWidth());
+    $this->assertEquals(250, $ads[0]->getHeight());
+    $this->assertEquals('http://example.com/ad', $ads[0]->getSource());
+
+    $article = InstantArticle::create();
+    $formatter->viewInstantArticle($entity->{$this->fieldName}, $article, Regions::REGION_CONTENT);
+
+    $children = $article->getChildren();
+    $this->assertEquals(1, count($children));
+    $this->assertTrue($children[0] instanceof Ad);
+    $this->assertEquals(300, $children[0]->getWidth());
+    $this->assertEquals(250, $children[0]->getHeight());
+    $this->assertEquals('http://example.com/ad', $children[0]->getSource());
+
+    // Test an embedded HTML ad.
+    $ad_html = '<script src="http://example.com/ad.js"></script>';
+    $entity->{$this->fieldName}->value = $ad_html;
+    $this->display->setComponent($this->fieldName, [
+      'type' => 'fbia_ad',
+      'settings' => [
+        'source_type' => FormatterBase::SOURCE_TYPE_HTML,
+      ],
+    ]);
+    $this->display->save();
+    /** @var \Drupal\fb_instant_articles\Plugin\Field\InstantArticleFormatterInterface $formatter */
+    $formatter = $this->display->getRenderer($this->fieldName);
+    $article = InstantArticle::create();
+    $formatter->viewInstantArticle($entity->{$this->fieldName}, $article, Regions::REGION_HEADER);
+
+    $ads = $article->getHeader()->getAds();
+    $this->assertEquals(1, count($ads));
+    $this->assertEquals(300, $ads[0]->getWidth());
+    $this->assertEquals(250, $ads[0]->getHeight());
+    $this->assertEquals($ad_html, $ads[0]->getHtml()->textContent);
+  }
+
+}
