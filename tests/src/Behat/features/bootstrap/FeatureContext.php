e91dd4e869e0bf86f5c8262de692fb9cf52ec3a1
Matthew Oliveira <m4olivei@gmail.com>
2017-10-30 13:38:31 -0400

    Add CircleCI testing scaffolding.

--- tests/src/Behat/features/bootstrap/FeatureContext.php
+++ tests/src/Behat/features/bootstrap/FeatureContext.php
@@ -6,16 +6,27 @@ use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
 
 /**
  * Defines application features used by all features.
+ *
+ * @codingStandardsIgnoreStart
  */
 class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   /**
-   * Install Facbook Instant Articles module.
+   * Install Facebook Instant Articles module.
    *
    * @BeforeSuite
    */
   public static function prepare(BeforeSuiteScope $scope) {
-    \Drupal::service('module_installer')->install(['fb_instant_articles_views']);
+    /** @var \Drupal\Core\Extension\ModuleHandler $moduleHandler */
+    $moduleHandler = \Drupal::service('module_handler');
+    if (!$moduleHandler->moduleExists('fb_instant_articles')) {
+      \Drupal::service('module_installer')->install(['fb_instant_articles']);
+    }
+
+    // Also uninstall the inline form errors module for easier testing.
+    if ($moduleHandler->moduleExists('inline_form_errors')) {
+      \Drupal::service('module_installer')->uninstall(['inline_form_errors']);
+    }
   }
 
 }
